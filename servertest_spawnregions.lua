35551737b505aeb138c89133f68a85e3ac55779a
Dishonor-dev <dishonxrr@gmail.com>
2025-04-15 22:44:56 -0600

    servertest files

---
+++ servertest_spawnregions.lua
@@ -0,0 +1,17 @@
+function SpawnRegions()
+	return {
+		{ name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
+		{ name = "Riverside, KY", file = "media/maps/Riverside, KY/spawnpoints.lua" },
+		{ name = "Rosewood, KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
+		{ name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
+		{ name = "BedfordFalls", file = "media/maps/BedfordFalls/spawnpoints.lua" },
+		{ name = "North", file = "media/maps/North/spawnpoints.lua" },
+		{ name = "South", file = "media/maps/South/spawnpoints.lua" },
+		{ name = "West", file = "media/maps/West/spawnpoints.lua" },
+		{ name = "Blackwood", file = "media/maps/Blackwood/spawnpoints.lua" },
+		{ name = "Grapeseed", file = "media/maps/Grapeseed/spawnpoints.lua" },
+		{ name = "ModTemplateWorld1", file = "media/maps/ModTemplateWorld1/spawnpoints.lua" },
+		{ name = "ModTemplateWorld2", file = "media/maps/ModTemplateWorld2/spawnpoints.lua" },
+		{ name = "RavenCreek", file = "media/maps/RavenCreek/spawnpoints.lua" },
+	}
+end
